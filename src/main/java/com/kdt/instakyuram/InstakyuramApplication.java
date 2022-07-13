f584c49e5cdf997d15f2b8a13f889e329ff021c8
pjh612 <pjh_jn@naver.com>
2022-07-13 16:04:20 +0900

    test : web hook 특정 브랜치 확인 테스트

--- src/main/java/com/kdt/instakyuram/InstakyuramApplication.java
+++ src/main/java/com/kdt/instakyuram/InstakyuramApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InstakyuramApplication {
+	//test 특정 브랜치 확인 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(InstakyuramApplication.class, args);
 	}
